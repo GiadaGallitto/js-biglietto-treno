3d6a4b309a510c295b27b62e6433e0afb710f3fe
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-21 16:28:59 +0100

    Fix js code and Add 40% discount, and total price

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 //     -Se il/la passegger* ha meno di 18 anni, allora verrà applicato uno sconto del 20% dal totale del prezzo base (Calcolato in km scelti)
 //         Altrimenti non verrà applicato lo sconto
 
-//     -Se il/la passegger* ha ha più di 65 anni, allora verrà applicato uno sconto del 40% dal totale del prezzo base (Calcolato in km scelti)
+//     -Se il/la passegger* ha più di 65 anni, allora verrà applicato uno sconto del 40% dal totale del prezzo base (Calcolato in km scelti)
 
 // -Prezzo finale da presentare:
 //     Emettere il totale con eventuali sconti già sottratti, con un massimo di due decimali (per indicare i centesimi sul prezzo)
@@ -23,23 +23,24 @@ console.log("Il prezzo base al chilometro è di: " +  priceForKm + " €" );
 
 const userKilometres = parseInt(prompt("Salve, quanti chilometri vorrebbe percorrere?"));
 // Devo fare in modo che l'utente possa mettere SOLO un valore a numero intero e non in caratteri
-const basePrice = (priceForKm * userKilometres);
+let ticketPrice = (priceForKm * userKilometres);
 
-console.log("Il prezzo base del biglietto è di: " + basePrice.toFixed(2) + " €");
+console.log("Il prezzo base del biglietto è di: " + ticketPrice.toFixed(2) + " €");
 
 const userAge = parseInt(prompt("Inserisca l'età del/della passeggero/a interessato/a"));
 // Devo fare in modo che l'utente possa mettere SOLO un valore a numero intero e non in caratteri
 console.log("L'età del/della passeggero/a è: " + userAge);
 
-const twentyDiscount = 20 * basePrice / 100;
-
 if (userAge < 18) {
-    const twentyDiscount = true
+    const twentyDiscount = 20 * ticketPrice / 100;
+    ticketPrice = ticketPrice - twentyDiscount;
+} else if (userAge > 65) {
+    const fortyDiscount = 40 * ticketPrice / 100;
+    ticketPrice = ticketPrice - fortyDiscount;
 } else {
-    const twentyDiscount = false
+
 }
 
-console.log("Lo sconto applicato sarà di: " + twentyDiscount.toFixed(2) + " €")
+console.log("Il prezzo del biglietto scontato è di: " + ticketPrice.toFixed(2) + " €");
 
-
-document.getElementById("output").innerHTML = "Il prezzo del suo biglietto corrisponde a: " ;
+document.getElementById("output").innerHTML = "Il prezzo del suo biglietto corrisponde a: " + ticketPrice.toFixed(2) + " &euro;" ;

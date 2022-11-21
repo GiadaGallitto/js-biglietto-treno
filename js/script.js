cf0c13ff2c58d3361d9e86e44681901d49e7cfa4
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-21 15:14:34 +0100

    Create a general outline for the phases of the ex.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+SCHEMA ESERCIZIO
+
+-Richiesta informazioni tramite prompt:
+    -Chiedo all'utente il numero di km che vuole percorrere
+    -Chiedo all'utente l'età del* passegger*
+
+-Calcolo base secondo i km:
+    -Moltiplico il numero di km fornito dall'utente, per 0.21 €
+
+-Eventuali sconti da sottrarre:
+    -Se il/la passegger* ha meno di 18 anni, allora verrà applicato uno sconto del 20% dal totale del prezzo base (Calcolato in km scelti)
+        Altrimenti non verrà applicato lo sconto
+
+    -Se il/la passegger* ha ha più di 65 anni, allora verrà applicato uno sconto del 40% dal totale del prezzo base (Calcolato in km scelti)
+
+-Prezzo finale da presentare:
+    Emettere il totale con eventuali sconti già sottratti, con un massimo di due decimali (per indicare i centesimi sul prezzo)
